ac2963ca3033ec0d50090323a9e01df3f47f8a7c
natpc <82532805+natpc@users.noreply.github.com>
2022-09-19 18:03:51 -0500

    first commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,12 @@
+<script>
+    let nombre="world"
+
+    let cambiarNombre = () => nombre="nat"
+    
+</script>
+
+<div  class="flex">
+    <h1 on:click={cambiarNombre} class="text-3xl shadow-xl font-bold bg-sky-600 text-white rounded-full px-3 py-2 text-center hover:bg-sky-800 m-4">
+        Hello {nombre}!
+      </h1>
+</div>
