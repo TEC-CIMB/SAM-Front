7ca5ff574e15abfac14d71797e36a486de0f4394
natpc <82532805+natpc@users.noreply.github.com>
2022-09-19 18:21:24 -0500

    Added Prettier plugin

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,12 +1,14 @@
 <script>
-    let nombre="world"
+  let nombre = "world";
 
-    let cambiarNombre = () => nombre="nat"
-    
+  let cambiarNombre = () => (nombre = "nat");
 </script>
 
-<div  class="flex">
-    <h1 on:click={cambiarNombre} class="text-3xl shadow-xl font-bold bg-sky-600 text-white rounded-full px-3 py-2 text-center hover:bg-sky-800 m-4">
-        Hello {nombre}!
-      </h1>
+<div class="flex">
+  <h1
+    on:click={cambiarNombre}
+    class="m-4 rounded-full bg-sky-600 px-3 py-2 text-center text-3xl font-bold text-white shadow-xl hover:bg-sky-800"
+  >
+    Hello {nombre}!
+  </h1>
 </div>
