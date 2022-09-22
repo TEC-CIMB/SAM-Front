aca7dcf617f4f529c5d8dcb4c8f41ed1a976527f
natpc <82532805+natpc@users.noreply.github.com>
2022-09-22 16:13:54 -0500

    2209-inicial

---
+++ src/routes/load/+page.svelte
@@ -0,0 +1,14 @@
+<script>
+  let nombre = "world";
+
+  let cambiarNombre = () => (nombre = "Dr. Rogelio y Claudia");
+</script>
+
+<div class="h-full w-full bg-[#ebf2fe]">
+  <h1
+    on:click={cambiarNombre}
+    class="m-4 rounded-full bg-sky-600 px-3 py-2 text-center text-3xl font-bold text-white shadow-xl hover:bg-sky-800"
+  >
+    Cargar {nombre}!
+  </h1>
+</div>
