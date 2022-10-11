a6202537f3eabf6f249bf36084b845d0dbd27ae3
natpc <82532805+natpc@users.noreply.github.com>
2022-10-11 15:01:19 -0500

    Prueba ubicación sensores

--- src/routes/home/Lower.svelte
+++ src/routes/home/Lower.svelte
@@ -1,103 +1,60 @@
-<div
-  class="flex h-screen w-full flex-col justify-center bg-center bg-no-repeat pt-4"
-  style="background-image: url(/lw.svg)"
->
-  <div class="basis-1/4 place-self-center font-bold text-[#022352f1]">
+<div class="flex h-screen w-full flex-col">
+  <div class="mt-4 place-self-center font-bold text-[#022352f1]">
     Front view
   </div>
+  <div class="relative">
+    <div><img class="mt-10 mb-10 w-full" src="lw.svg" alt="lower" /></div>
 
-  <div class="flex basis-1/4 flex-row">
-    <div class="ml-32 mt-20 pl-2">
+    <div
+      class="absolute top-0 grid h-full w-full grid-cols-2 place-content-around"
+    >
       <input
         id="RadioSensors"
         type="radio"
-        value="Rfemur"
+        value="Rshl"
         name="bordered-radio2"
-        class="border-gray-300 bg-gray-100 text-blue-600"
+        class="absolute right-32 bottom-20 border-gray-300 bg-gray-100 text-blue-600"
       />
-    </div>
-    <div class="ml-40 mt-20 w-full">
       <input
         id="RadioSensors"
         type="radio"
-        value="Lfemur"
+        value="Rshl"
         name="bordered-radio2"
-        class="border-gray-300 bg-gray-100 text-blue-600"
+        class="absolute left-7 right-48 bottom-20 border-gray-300 bg-gray-100 pb-48 text-blue-600"
       />
-    </div>
-  </div>
-
-  <div class="flex basis-1/4 flex-row">
-    <div class="mt-24 ml-32 w-full">
       <input
         id="RadioSensors"
         type="radio"
-        value="Rknee"
+        value="Rshl"
         name="bordered-radio2"
-        class="border-gray-300 bg-gray-100 text-blue-600"
+        class="absolute right-32 bottom-48 border-gray-300 bg-gray-100 text-blue-600"
       />
-    </div>
-
-    <div class="mt-24 ml-16 w-full">
       <input
         id="RadioSensors"
         type="radio"
-        value="Lknee"
+        value="Rshl"
         name="bordered-radio2"
-        class="border-gray-300 bg-gray-100 text-blue-600"
+        class="absolute bottom-48 right-72 left-32 border-gray-300 bg-gray-100 text-blue-600"
       />
-    </div>
-  </div>
-
-  <div class="flex basis-1/4 flex-row">
-    <div class="mt-10 ml-28 w-full pl-2">
       <input
         id="RadioSensors"
         type="radio"
-        value="Rankle"
+        value="Rshl"
         name="bordered-radio2"
-        class="border-gray-300 bg-gray-100 text-blue-600"
+        class="absolute top-40 left-36 border-gray-300 bg-gray-100 text-blue-600"
       />
-    </div>
-    <div class="mt-10 ml-20 w-full pl-2">
       <input
         id="RadioSensors"
         type="radio"
-        value="Lankle"
+        value="Rshl"
         name="bordered-radio2"
-        class="border-gray-300 bg-gray-100 text-blue-600"
+        class="absolute left-64 right-20 top-40 border-gray-300 bg-gray-100 text-blue-600"
       />
     </div>
   </div>
 </div>
-<div
-  class="flex h-screen w-full flex-col justify-center bg-center bg-no-repeat pt-4"
-  style="background-image: url(/lw.svg)"
->
-  <div class="basis-1/5">
-    <div class="flex flex-col">
-      <div class="place-self-center font-bold text-[#022352f1]">Back view</div>
-      <div class="flex flex-row">
-        <div class="mt-9 ml-48 w-full">
-          <input
-            id="RadioSensors"
-            type="radio"
-            value="Lhip"
-            name="bordered-radio2"
-            class="border-gray-300 bg-gray-100 text-blue-600"
-          />
-        </div>
-        <div class="mt-9 mr-32 w-full">
-          <input
-            id="RadioSensors"
-            type="radio"
-            value="Rhip"
-            name="bordered-radio2"
-            class="border-gray-300 bg-gray-100 text-blue-600"
-          />
-        </div>
-      </div>
-    </div>
-  </div>
-  <div class="basis-4/5" />
+
+<div class="flex h-screen w-full flex-col">
+  <div class="mt-4 place-self-center font-bold text-[#022352f1]">Back view</div>
+  <img class="mt-10 mb-10 w-full" src="lw.svg" alt="lower" />
 </div>
